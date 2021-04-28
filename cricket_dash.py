67bd874778fa544a8ce97ff9b63d1e5578ff13a7
Nishant <nishantsinghsiddhu@yahoo.com>
2021-04-29 02:25:54 +0530

    changes to.py file

--- cricket_dash.py
+++ cricket_dash.py
@@ -4,50 +4,87 @@ import plotly.express as px
 import dash 
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
 
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
 
-app.layout = html.Div([
+app.layout = dbc.Container(
+    [
+        dbc.Row([
+            dbc.Col([
+                html.H2("Players Performance Dashboard (India Domestic)"),
+                html.H5("Nishant Singh Siddhu"),
+            ], width={'size': 9, 'offset':2}),]),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col([
+                html.Div([
+                    html.H5("Key Parameters"),
+                    html.P("Select the Series"),
+                    
+                    dbc.DropdownMenu(
+                                id='n_trophy',
+                                label="Select",
+                                children=[
+                                    dbc.DropdownMenuItem("SYED MUSHTAQ ALI TROPHY",id="dropdown_smat"),
+                                    dbc.DropdownMenuItem("VIJAY HAZARE TROPHY",id="dropdown_vjt")],
+                                    style={"display": "flex", "flexWrap": "wrap"}),
+                    html.Hr(),
+                    html.P("Select the year"), 
+                    dcc.Slider(
+                    id='year',
+                    min=2019,
+                    max=2021,
+                    value=2021,
+                        marks={
+                            2019: '2019',
+                            2020: '2020',
+                            2021: '2021',
+                            },included=False),
+                ]),
+                html.Hr(),
+                html.Div([
+                    html.H5("Select Batsmen type"),
+                    dbc.Button("Openers", id="display_op", color="dark", style={"margin": "5px"},
+                               n_clicks_timestamp='0'),
+                    dbc.Button("Middle Order", id="display_mo", color="dark", style={"margin": "5px"},
+                               n_clicks_timestamp='0'),
+                    dbc.Button("Lower Middle Order", id="display_lmo", color="dark",
+                               style={"margin": "5px"}, n_clicks_timestamp='0'),
+                    dbc.Button("Wicket keepers", id="display_wkb", color="dark",
+                               style={"margin": "5px"}, n_clicks_timestamp='0'),
+                ]),
+                html.Hr(),
 
-    html.H1("Players Performances Dashboard (India Domestic)", style={'text-align': 'center'}),
+            ], width=3),
 
-    html.P("Select a Series", style={'text-align': 'left'}),
-    dcc.Dropdown(
-    options=[
-        {'label': 'SYED MUSHTAQ ALI TROPHY', 'value': 'SMAT'},
-        {'label': 'VIJAY HAZARE TROPHY', 'value': 'VHT'}],
-        multi=False,
-        style={'width': "40%"},
-        placeholder="Select a Series",),
+            dbc.Col([
+                # html.Div(id='display')
+                dbc.Spinner(
+                    dcc.Graph(id='display', style={'height': '80vh'}),
+                    color="primary"
+                )
+            ], width=True)
 
-    html.Div([ dcc.Slider(
-    min=2019,
-    max=2021,
-    value=2021,
-    marks={
-        2019: {'label': '2019'},
-        2020: {'label': '2020'},
-        2021: {'label': '2021'},
-    },
-    included=False),
+        ]),
+        html.Hr(),
+        html.P([
+            html.A("Source code", href="https://github.com/iambolt/cricket_scouting_dashboard"),
+            ". Build beautiful UIs for your scientific computing apps with ",
+            html.A("Plot.ly ", href="https://plotly.com/"),
+            "and ",
+            html.A("Dash", href="https://plotly.com/dash/"),
+            "!",
+        ]),
     ],
-    style={'width': "40%",'padding-left':'25%', 'padding-right':'25%'}),
-  
+    fluid=True
+)
 
-    
-
-
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-
-
-    dcc.Graph(id='my_bee_map', figure={})
-
-])
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True,use_reloader=False)
